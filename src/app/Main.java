55b53cfc199091c3505f3e3034f9b9616fa6dc5e
Sadiq Ibrahim <your-github-email@example.com>
2026-04-21 12:35:03 +0100

    feat : backend final fix

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,12 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"app", "controllers", "data", "dtos", "exceptions", "services", "utils"})
